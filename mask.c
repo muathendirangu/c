576ba6ddce235e3d3d54f4a7f9d3cf3f8314e59a
muathendirangu <muathe.ndirangu@gmail.com>
2023-11-12 19:01:35 +0300

    store two 4-bit variables in one 8-bit variable

---
+++ mask.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define LOWER_MASK 0X0F // Binary 0000 1111
+#define UPPER_MASK 0XF0 // Binary 1111 0000
+
+int main(int argc, char **argv)
+{
+    //two 4-bit variables stored in one 8-bit variable
+    char value = 0xA5; // Binary 1010 0101
+    char lower = value & LOWER_MASK; // Extract lower 4 bits
+    char upper = (value & UPPER_MASK) >> 4; // Extract upper 4 bits
+
+    printf("Lower 4 bits: %x\n", lower);
+    printf("Upper 4 bits: %x\n", upper);
+}
